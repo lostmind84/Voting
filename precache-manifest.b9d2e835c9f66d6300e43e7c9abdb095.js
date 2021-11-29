63917a1d9f143aa0d2a871ac76a9d00e763d691f
Fabien F <dakeyras.crypto@gmail.com>
2021-11-29 16:38:45 +0100

    Updates

---
+++ precache-manifest.b9d2e835c9f66d6300e43e7c9abdb095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2d442677bc9518aaf096238c8e23ca",
+    "url": "/Developpeur-Ethereum-Template/index.html"
+  },
+  {
+    "revision": "cbcc6ce11b9be7761ecb",
+    "url": "/Developpeur-Ethereum-Template/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "cdb6fffb2ee83d705eb5",
+    "url": "/Developpeur-Ethereum-Template/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cbcc6ce11b9be7761ecb",
+    "url": "/Developpeur-Ethereum-Template/static/js/2.541bb8ed.chunk.js"
+  },
+  {
+    "revision": "cdb6fffb2ee83d705eb5",
+    "url": "/Developpeur-Ethereum-Template/static/js/main.fb83ceaf.chunk.js"
+  },
+  {
+    "revision": "2e0a450c7b47610fa78e",
+    "url": "/Developpeur-Ethereum-Template/static/js/runtime-main.9ae799f3.js"
+  }
+]);
